d0d9259b5be2549d92a72892459d94da6ebf2b5d
kapka videnova <videnovakapka@gmail.com>
2022-03-07 16:20:11 +0200

    Created a project

---
+++ BabyItems/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BabyItems.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
