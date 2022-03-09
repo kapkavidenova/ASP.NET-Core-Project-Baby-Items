4303c3c9eec0f2840ef1106cbd9b7b88f5703128
kapka videnova <videnovakapka@gmail.com>
2022-03-09 17:46:37 +0200

    Added entities

---
+++ BabyItems.Infrastructure/Data/Models/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static BabyItems.Infrastructure.DataConstants;
+
+namespace BabyItems.Infrastructure.Data.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; }
+
+        public int ForWeight { get; set; }
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(ModelMaxLength)]
+        public string Model { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public string Dimensions { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
---
+++ BabyItems.Infrastructure/DataConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyItems.Infrastructure
+{
+    public class DataConstants
+    {
+        public const int NameMaxLength = 50;
+        public const int DescriptionMaxLength = 1000;
+        public const int DescriptionMinLength = 5;
+
+        public const int ModelMaxLength = 20;
+        //public const int ModelMinLength = 2;
+
+        public const int CategoryMaxLength = 20;
+    }
+}
