684d87ef748cadba52997f091608861824d797a9
kapka videnova <videnovakapka@gmail.com>
2022-03-11 17:06:00 +0200

    Login and Register forms

--- BabyItems/Controllers/HomeController.cs
+++ BabyItems/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace BabyItems.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
